4a856c3a1db9c55f3d05bf242b24fc73d24575db
Li Dong <lidong8@le.com>
2017-06-15 17:18:25 +0800

    添加hadoop conf环境变量

--- hadoop/.env.sh
+++ hadoop/.env.sh
@@ -1,6 +1,8 @@
 # !/bin/bash
 
 export HADOOP_HOME=`pwd`/hadoop-2.8.0
+export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
+
 export PATH=$HADOOP_HOME/bin:$HADOOP_HOME/sbin:$PATH
 
 make_opt_link hadoop $HADOOP_HOME
