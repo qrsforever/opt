b275d3e64ae83e8aad8b8ae0c4299d14a079f695
Li Dong <lidong8@le.com>
2017-07-20 10:15:07 +0800

    更新jdk, 使用jinfo

--- jdk/.env.sh
+++ jdk/.env.sh
@@ -1,6 +1,7 @@
 # !/bin/bash
 
-export JAVA_HOME=`pwd`/jdk1.8.0_73
+export JAVA_VERSION=1.8.0_141
+export JAVA_HOME=`pwd`/jdk${JAVA_VERSION}
 export JRE_HOME=$JAVA_HOME/jre
 export CLASSPATH=.:$JAVA_HOME/lib:$JRE_HOME/lib
 export PATH=$JAVA_HOME/bin:$PATH
