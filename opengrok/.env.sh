0b6cc7e5c9d0c094e6d3846db71abab2f6f34c12
Li Dong <lidong8@le.com>
2017-07-28 15:40:28 +0800

    1. add opengrok and tomcat 2. mod 0gvim

---
+++ opengrok/.env.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# export CATALINA_HOME=$TOMCAT_HOME
+export OPENGROK_TOMCAT_BASE=/opt/tomcat
+export OPENGROK_VERSION=1.1-rc7
+export OPENGROK_HOME=`pwd`/opengrok-${OPENGROK_VERSION}
+export OPENGROK_INSTANCE_BASE=$OPENGROK_HOME
+export PATH=$OPENGROK_HOME/bin:$PATH
+
+# mkdir -p $OPENGROK_INSTANCE_BASE/src
+# mkdir -p $OPENGROK_INSTANCE_BASE/data
+# mkdir -p $OPENGROK_INSTANCE_BASE/etc
+# ./OpenGrok deploy
+# ./OpenGrok index
+
+# 也可以 每个项目可以指定一个配置
+# export OPENGROK_CONFIGURATION=xxx/src/softlink/opengrok.conf
+# cat opengrok.conf
+#   OPENGROK_APP_SERVER=Tomcat
+#   OPENGROK_TOMCAT_BASE=/usr/local/Cellar/tomcat/8.0.21/libexec
+#   OPENGROK_SCAN_DEPTH=4
+#   OPENGROK_VERBOSE=yes
+#   OPENGROK_PROGRESS=yes
+#   IGNORE_PATTERNS="-i f:foo.txt -i *.bar d:target -i d:.tox"
+
+make_opt_link opengrok $OPENGROK_HOME
--- tomcat/.env.sh
+++ tomcat/.env.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-export TOMCAT_HOME=`pwd`/apache-tomcat-8.5.14
+export TOMCAT_VERSION=8.5.14
+export TOMCAT_HOME=`pwd`/apache-tomcat-${TOMCAT_VERSION}
+export CATALINA_HOME=$TOMCAT_HOME
 export PATH=$TOMCAT_HOME/bin:$PATH
 
 make_opt_link tomcat $TOMCAT_HOME
