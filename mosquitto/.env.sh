6512e2331138d5839620f30bdb14842e5921decc
Li Dong <lidong8@le.com>
2018-02-28 13:17:29 +0800

    修改contab任务

--- mosquitto/.env.sh
+++ mosquitto/.env.sh
@@ -3,6 +3,9 @@
 export MOSQUITTO_VERSION=1.4.14
 export MOSQUITTO_HOME=`pwd`/mosquitto-${MOSQUITTO_VERSION}
 
+# sudo apt-get install libc-ares-dev
+# sudo apt-get install libc-ares2
+# sudo apt-get install uuid-dev
 # cd $MOSQUITTO_HOME
 # make
 # sudo make install
