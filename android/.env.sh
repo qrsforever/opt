674d0981af01b2b69fe39b0c7c3f1e80640bbf6a
Li Dong <lidong8@le.com>
2019-05-16 22:53:44 +0800

    android env

--- android/.env.sh
+++ android/.env.sh
@@ -3,7 +3,6 @@
 make_opt_link sdk `pwd`/sdk
 
 export ANDROID_HOME=/opt/sdk
-export JNI_HOME=$ANDROID_HOME/../jni
 export SDK_HOME=$ANDROID_HOME
 export PATH=$SDK_HOME/emulator:$SDK_HOME/platform-tools:$SDK_HOME/tools/bin:$PATH
 
